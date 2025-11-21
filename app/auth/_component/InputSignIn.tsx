6b531608a3f632a7fda0d35810d53a9d9627c722
rold dev <cvuyante24@gmail.com>
2025-11-21 21:28:33 +0800

    feat: added spinner to sign in/sign up button

--- app/auth/_component/InputSignIn.tsx
+++ app/auth/_component/InputSignIn.tsx
@@ -1,110 +1,129 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { SignInFormValues, SignInSchema } from '../_lib/auth-zod';
-import { authClient } from '@/lib/auth-client';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignInFormValues, SignInSchema } from "../_lib/auth-zod";
+import { authClient } from "@/lib/auth-client";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { toast } from 'sonner';
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+import { toast } from "sonner";
 
 // import { signIn } from '@/lib/actions/auth-actions';
 
 export default function InputSignIn() {
-	const [error, setError] = useState<string | null>(null);
-	const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
-	const router = useRouter();
+    const router = useRouter();
 
-	const form = useForm<SignInFormValues>({
-		resolver: zodResolver(SignInSchema), // RHF will use Zod's rules for validation
-		defaultValues: {
-			email: '',
-			password: ''
-		}
-	});
+    const form = useForm<SignInFormValues>({
+        resolver: zodResolver(SignInSchema), // RHF will use Zod's rules for validation
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
 
-	// add enter key event when pressing that
-	async function onSubmit(values: SignInFormValues) {
-		setError(null);
-		setLoading(true);
+    // add enter key event when pressing that
+    async function onSubmit(values: SignInFormValues) {
+        setError(null);
+        setLoading(true);
 
-		// await signIn(values.email, values.password);
-		const { email, password } = values;
-		const { error } = await authClient.signIn.email({
-			email,
-			password,
-			callbackURL: '/dashboard'
-		});
+        // await signIn(values.email, values.password);
+        const { email, password } = values;
+        const { error } = await authClient.signIn.email({
+            email,
+            password,
+            callbackURL: "/dashboard",
+        });
 
-		setLoading(false);
-		if (error) {
-			setError(error.message || 'something went wrong');
-		} else {
-			toast.success('sign in successfully');
-			router.push('/dashboard');
-		}
-	}
-	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-				<FormField
-					control={form.control}
-					name='email'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<FormControl>
-								<Input placeholder='Enter your email...' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+        setLoading(false);
+        if (error) {
+            setError(error.message || "something went wrong");
+            toast.warning("sign in failed");
+        } else {
+            toast.success("sign in successfully");
+            router.push("/dashboard");
+        }
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your email..."
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-				<FormField
-					control={form.control}
-					name='password'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Password</FormLabel>
-							<FormControl>
-								<Input
-									type='password'
-									placeholder='Enter your password...'
-									onKeyDown={(event) => {
-										if (event.key === 'Enter') {
-											// Prevent the default behavior (in case it submits twice or you
-											// want to control the flow completely)
-											event.preventDefault();
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type="password"
+                                    placeholder="Enter your password..."
+                                    onKeyDown={(event) => {
+                                        if (event.key === "Enter") {
+                                            // Prevent the default behavior (in case it submits twice or you
+                                            // want to control the flow completely)
+                                            event.preventDefault();
 
-											// Manually trigger the form submission via react-hook-form
-											form.handleSubmit(onSubmit)();
-										}
-									}}
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<Button type='submit' className='w-full'>
-					Sign In
-				</Button>
-			</form>
-		</Form>
-	);
+                                            // Manually trigger the form submission via react-hook-form
+                                            form.handleSubmit(onSubmit)();
+                                        }
+                                    }}
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage>
+                                {error && error.includes("password")
+                                    ? "Invalid password. Please try again."
+                                    : ""}
+                            </FormMessage>
+                        </FormItem>
+                    )}
+                />
+                <Button
+                    disabled={loading ? true : false}
+                    type="submit"
+                    className="w-full"
+                >
+                    {loading ? (
+                        <>
+                            <Spinner /> Sign in
+                        </>
+                    ) : (
+                        "Sign In"
+                    )}
+                </Button>
+            </form>
+        </Form>
+    );
 }
--- app/auth/_component/InputSignUp.tsx
+++ app/auth/_component/InputSignUp.tsx
@@ -1,116 +1,139 @@
-'use client';
+"use client";
 
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { SignUpFormValues, SignUpSchema } from '../_lib/auth-zod';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignUpFormValues, SignUpSchema } from "../_lib/auth-zod";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 // import { signup } from '@/lib/actions/auth-actions';
-import { authClient } from '@/lib/auth-client';
-import { useState } from 'react';
-import { toast } from 'sonner';
-import { useRouter } from 'next/navigation';
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function InputSignUp() {
-	const [error, setError] = useState<string | null>(null);
-	const router = useRouter();
-	const form = useForm<SignUpFormValues>({
-		resolver: zodResolver(SignUpSchema), // RHF will use Zod's rules for validation
-		defaultValues: {
-			name: '',
-			email: '',
-			password: ''
-		}
-	});
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const router = useRouter();
+    const form = useForm<SignUpFormValues>({
+        resolver: zodResolver(SignUpSchema), // RHF will use Zod's rules for validation
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    });
 
-	const onSubmit = async (values: SignUpFormValues) => {
-		setError(null);
-		// this way using SERVER ACTION
-		// await signup(values.name, values.email, values.password);
-		const { name, email, password } = values;
-		// callbackUrl must replace "email-verified path" is the page of that is done
-		const { error } = await authClient.signUp.email({
-			name,
-			email,
-			password,
-			callbackURL: '/dashboard'
-		});
+    const onSubmit = async (values: SignUpFormValues) => {
+        setError(null);
+        setLoading(true);
+        // this way using SERVER ACTION
+        // await signup(values.name, values.email, values.password);
+        const { name, email, password } = values;
+        // callbackUrl must replace "email-verified path" is the page of that is done
+        const { error } = await authClient.signUp.email({
+            name,
+            email,
+            password,
+            callbackURL: "/dashboard",
+        });
 
-		if (error) {
-			setError(error.message || 'something went wrong');
-		} else {
-			toast.success('sign up successfully');
-			router.push('/dashboard');
-		}
-	};
-	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-				<FormField
-					control={form.control}
-					name='name'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Fullname</FormLabel>
-							<FormControl>
-								<Input placeholder='Enter your Fullname' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+        setLoading(false);
+        if (error) {
+            setError(error.message || "something went wrong");
+        } else {
+            toast.success("sign up successfully");
+            router.push("/dashboard");
+        }
+    };
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Fullname</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your Fullname"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-				<FormField
-					control={form.control}
-					name='email'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<FormControl>
-								<Input placeholder='Enter your email' {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your email"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-				<FormField
-					control={form.control}
-					name='password'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Password</FormLabel>
-							<FormControl>
-								<Input
-									placeholder='Enter your password'
-									type='password'
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your password"
+                                    type="password"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-				{error && (
-					<div role='alert' className='text-sm text-red-600'>
-						{error}
-					</div>
-				)}
+                {error && (
+                    <div role="alert" className="text-sm text-red-600">
+                        {error}
+                    </div>
+                )}
 
-				<Button type='submit' className='w-full'>
+                {/* <Button type='submit' className='w-full'>
 					Sign Up
-				</Button>
-			</form>
-		</Form>
-	);
+				</Button> */}
+                <Button
+                    disabled={loading ? true : false}
+                    type="submit"
+                    className="w-full"
+                >
+                    {loading ? (
+                        <>
+                            <Spinner /> Sign in
+                        </>
+                    ) : (
+                        "Sign In"
+                    )}
+                </Button>
+            </form>
+        </Form>
+    );
 }
