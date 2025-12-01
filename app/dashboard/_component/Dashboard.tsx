c719513ebf9c0fd4eea4333e0b837114f2640119
rold dev <cvuyante24@gmail.com>
2025-12-01 22:31:23 +0800

    feat: added edit and delete

--- app/dashboard/_component/Dashboard.tsx
+++ app/dashboard/_component/Dashboard.tsx
@@ -1,53 +1,48 @@
-'use client';
+"use client";
 
-import React, { useEffect } from 'react';
-import { BookBorrow } from './BookBorrow';
-import { useDataStore } from '@/lib/store';
-import DashboardHeader from '@/components/DashboardHeader';
-import { useFetchBooks } from '../_lib/useBooks';
+import { Suspense } from "react";
+import { BookBorrow } from "./BookBorrow";
+import DashboardHeader from "@/components/DashboardHeader";
+import SkeletonDashboard from "./skeleton/SkeletonDashboard";
 
 export default function Dashboard() {
-	const { setBooks } = useDataStore();
-	const books = useDataStore((state) => state.bookData);
-	const { data: bookData, isLoading, isError, error } = useFetchBooks();
+    return (
+        <div className="space-y-4">
+            <header className="max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <DashboardHeader title="Barrowed Books" quantity={200} />
+                <DashboardHeader title="Total Books" quantity={499} />
+                <DashboardHeader title="Total Users" quantity={700} />
+            </header>
 
-	useEffect(() => {
-		if (bookData.length > 0) setBooks(bookData);
-	}, [bookData, setBooks]);
+            <section className="max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-4 ">
+                <div className="grid grid-cols-1 gap-4">
+                    <Suspense fallback={<SkeletonDashboard />}>
+                        <BookBorrow
+                            title="Borrow Requests"
+                            createBook={false}
+                            limit={3}
+                        />
+                    </Suspense>
 
-	return (
-		<div className='space-y-4'>
-			<header className='max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-				<DashboardHeader title='Barrowed Books' quantity={200} />
-				<DashboardHeader title='Total Books' quantity={499} />
-				<DashboardHeader title='Total Users' quantity={700} />
-			</header>
+                    <Suspense fallback={<SkeletonDashboard />}>
+                        <BookBorrow
+                            title="Account Resquested"
+                            createBook={false}
+                            limit={0}
+                        />
+                    </Suspense>
+                </div>
 
-			<section className='max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-4 '>
-				<div className='grid grid-cols-1 gap-4'>
-					<BookBorrow
-						title='Borrow Requests'
-						createBook={false}
-						books={books}
-						limit={3}
-					/>
-					<BookBorrow
-						title='Account Resquested'
-						createBook={false}
-						books={books}
-						limit={0}
-					/>
-				</div>
-
-				<div>
-					<BookBorrow
-						title='Recently Added Books'
-						createBook={true}
-						books={books}
-						limit={6}
-					/>
-				</div>
-			</section>
-		</div>
-	);
+                <div>
+                    <Suspense fallback={<SkeletonDashboard />}>
+                        <BookBorrow
+                            title="Recently Added Books"
+                            createBook={true}
+                            limit={6}
+                        />
+                    </Suspense>
+                </div>
+            </section>
+        </div>
+    );
 }
--- context/provider.tsx
+++ context/provider.tsx
@@ -1,13 +1,15 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-	// Create QueryClient on client side only
-	const [queryClient] = useState(() => new QueryClient());
+    // Create QueryClient on client side only
+    const [queryClient] = useState(() => new QueryClient());
 
-	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-	);
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
 }
