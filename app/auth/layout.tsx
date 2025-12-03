af665c46f5922145217f3555079a0f82a5d69815
rold dev <52446872+haroldm2598@users.noreply.github.com>
2025-12-03 19:17:32 +0800

    fix: removing auth import

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,36 +1,34 @@
-import { ReactNode } from "react";
-import { Metadata } from "next";
+import { ReactNode } from 'react';
+import { Metadata } from 'next';
 
-import { unauthorized } from "next/navigation";
+import { getServerSession } from '@/lib/auth-get-sessions';
 
-import { getServerSession } from "@/lib/auth-get-sessions";
-
-import SessionSigInWrapper from "@/components/SessionSignInWrapper";
-import { Toaster } from "@/components/ui/sonner";
-import Navbar from "@/components/Navbar";
+import SessionSigInWrapper from '@/components/SessionSignInWrapper';
+import { Toaster } from '@/components/ui/sonner';
+import Navbar from '@/components/Navbar';
 
 interface RootLayoutProps {
-    children: ReactNode;
+	children: ReactNode;
 }
 
 export const metadata: Metadata = {
-    title: {
-        default: "Welcome to BetterAuth",
-        template: "%s | BetterAuth.js",
-    },
-    description: "Hello dynamic and responsive friendly.",
+	title: {
+		default: 'Welcome to BetterAuth',
+		template: '%s | BetterAuth.js'
+	},
+	description: 'Hello dynamic and responsive friendly.'
 };
 
 export default async function AuthLayout({ children }: RootLayoutProps) {
-    const session = await getServerSession();
+	const session = await getServerSession();
 
-    return (
-        <div className="bg-blue-50 min-h-dvh">
-            <SessionSigInWrapper redirectUrl="/dashboard">
-                <Navbar session={session} />
-                {children}
-                <Toaster />
-            </SessionSigInWrapper>
-        </div>
-    );
+	return (
+		<div className='bg-blue-50 min-h-dvh'>
+			<SessionSigInWrapper redirectUrl='/dashboard'>
+				<Navbar session={session} />
+				{children}
+				<Toaster />
+			</SessionSigInWrapper>
+		</div>
+	);
 }
